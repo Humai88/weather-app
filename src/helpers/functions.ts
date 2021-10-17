75559d3adab6c4fd2f7fad4b744c4133386ec988
Gumay <humaivier88@gmail.com>
2021-10-17 20:20:18 +0100

    Add month

--- src/helpers/functions.ts
+++ src/helpers/functions.ts
@@ -1,6 +1,46 @@
 export const editTimeString = (str: string) => {
   let arr = str.split(" ");
-  let day = arr[0].split("-").slice(1).reverse().join(".");
+  let day = arr[0].split("-").slice(1);
+  switch (day[0]) {
+    case "01":
+      day[0] = "Jan";
+      break;
+    case "02":
+      day[0] = "Feb";
+      break;
+    case "03":
+      day[0] = "Mar";
+      break;
+    case "04":
+      day[0] = "Apr";
+      break;
+    case "05":
+      day[0] = "May";
+      break;
+    case "06":
+      day[0] = "June";
+      break;
+    case "07":
+      day[0] = "July";
+      break;
+    case "08":
+      day[0] = "Aug";
+      break;
+    case "09":
+      day[0] = "Sept";
+      break;
+    case "10":
+      day[0] = "Oct";
+      break;
+    case "11":
+      day[0] = "Nov";
+      break;
+    case "12":
+      day[0] = "Dec";
+      break;
+  }
+  let finalDay = day.join(", ");
+
   let time = arr[1].split(":").slice(0, -1).join(":");
-  return `${day} ${time}`;
+  return `${finalDay} ${time}`;
 };
